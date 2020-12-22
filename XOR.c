46079078dc54027d85bcbfd1992b6d927c691730
MrSn0w1337 <64164734+MrSn0w1337@users.noreply.github.com>
2020-12-22 14:29:23 +0100

    Add files via upload

---
+++ XOR.c
@@ -0,0 +1,38 @@
+#include "Includes.h"
+
+int XOR(int Value)
+{
+	if (Value != szKeyLen)
+		return Value ^ szKeyLen;
+	else
+		return Value ^ (szKeyLen + 1);
+}
+
+void SZXOR(char* buffer, int len, int szLen, BOOL Encrypt)
+{
+	if (Encrypt)
+	{
+		if (!szLen) return;
+		memset(buffer + szLen, 0, len - szLen);
+		buffer[szLen] = '\0';
+	}
+
+	int z = 0;
+	for (int i = 0; i < len; i++)
+	{
+		if (i >= szKeyLen)
+			z = 0;
+		else
+			z = i;
+
+		if (buffer[i] == szKey[i] || buffer[i] == (szKey[z] ^ ~szKey[z]))
+		{
+			if (buffer[i] == '\0') break;
+			buffer[i] = buffer[i] ^ (~szKey[z]);
+			continue;
+		}
+
+		if (buffer[i] == '\0') break;
+		buffer[i] ^= szKey[z];
+	}
+}
---
+++ XOR.h
@@ -0,0 +1,7 @@
+#pragma once
+
+int XOR(int Value);
+void SZXOR(char* buffer, int len, int szLen, BOOL Encrypt);
+
+char szKey[NAME_SIZE];
+int szKeyLen;
